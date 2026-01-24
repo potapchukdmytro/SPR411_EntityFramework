a896fdedfb91a0931e273427cee0996eb41fb973
potapchukdmytro <traiget@gmail.com>
2026-01-24 10:42:32 +0200

    first commit

---
+++ 01_intro/01_intro/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _01_intro
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server=localhost;Database=SPR411;Trusted_Connection=True;TrustServerCertificate=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ 01_intro/01_intro/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
